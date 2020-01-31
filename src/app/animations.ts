69464855129171f662903051f96ce1598c3efae2
Ryan Racicot <ryan@dhcp-10-5-10-51.wireless.rochester.edu>
2020-01-31 12:51:38 -0500

    Staggered fade in animations

--- src/app/animations.ts
+++ src/app/animations.ts
@@ -1,21 +1,79 @@
-import { trigger, transition, style, query, animate, animateChild, group, state, keyframes } from "@angular/animations";
+import { trigger, transition, style, query, animate, animateChild, group, state, keyframes, stagger } from "@angular/animations";
+
+let animationTimeLength = '250ms'
 
 export const pageNavigationAnimation =
   trigger('routeAnimations', [
-    transition('* <=> *', [
-      // Set a default  style for enter and leave
-      query(':enter, :leave', [
-        style({
-          position: 'absolute',
-          width: '100%',
-          height: '100%',
-          opacity: 0,
-        }),
-      ], { optional: true } ),
-      // Animate the new page in
-      query(':enter', [
-        animate('250ms', style({ opacity: 1 })),
-        // animate('600ms ease', style({ opacity: 1, transform: 'scale(1) translateY(0)' })),
-      ], { optional: true})
-    ]),
+
+    ProjectsPageTransition(),
+    DefaultPageTransition(),
+
 ]);
+
+function DefaultPageTransition() {
+  return transition('* <=> *', [
+
+    query(':enter, :leave', [
+      style({
+        position: 'absolute',
+        width: '100%',
+        height: '100%',
+        opacity: 0,
+        transform: 'scale(0)'
+      }),
+    ], { optional: true } ),
+
+    query(':leave', [
+      style({opacity: 1, transform: 'scale(1)'}),
+      animate(animationTimeLength + ' ease', style({ opacity: 0, transform: 'scale(0)' })),
+    ], { optional: true}),
+    query(':enter', [
+      animate(animationTimeLength + ' ease', style({ opacity: 1, transform: 'scale(1)' })),
+    ], { optional: true}),
+
+  ]);
+}
+
+function ProjectsPageTransition() {
+  return transition('* => ProjectsPage', [
+
+    query(':enter, :leave', [
+      style({
+        position: 'absolute',
+        width: '100%',
+        height: '100%',
+        opacity: 0,
+        transform: 'scale(0)'
+      }),
+    ], { optional: true } ),
+
+    query(':enter .card', [
+      style({
+        opacity: 0,
+      }),
+    ]),
+
+    query(':leave', [
+      style({opacity: 1, transform: 'scale(1)'}),
+      animate(animationTimeLength + ' ease', 
+        style({ 
+          opacity: 0,
+          transform: 'scale(0)' 
+        })
+      ),
+    ], { optional: true}),
+
+    query(':enter', [
+      group([
+        animate(animationTimeLength + ' ease', style({ opacity: 1, transform: 'scale(1)' })),
+        query(':enter .card', stagger(100, [
+          animate('350ms', 
+            style({
+              opacity: 1,
+            }))
+          ]),
+        ),
+      ])
+    ], { optional: true}),
+  ]);
+}
